3a2b5cd956e7a1f8248ccdad4626c0fa37ff460a
kame <kamelop119@gmail.com>
2016-01-13 21:06:56 +0900

    make commandline timer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 
 
 public class Main {
-
     public static void main(String[] args) {
+
         RunTimer runtimer = new RunTimer();
         runtimer.time();
     }
--- src/com/company/TimeClass.java
+++ src/com/company/TimeClass.java
@@ -22,26 +22,52 @@ class TimeClass extends TimerTask{
     }
     //Timer.schedule()が呼び出された時にすること。
     public void run(){
+        //秒数を表示する
         second++;
-        System.out.println("time" + second);
+        if(second != 1) System.out.print("\r");
+        System.out.print(second);
+    }
+
+    public int getSecond(){
+        return second;
     }
 
 }
 
 class RunTimer{
     //タイマー本体
-    public void time() {
+    public void time(){
         Timer timer = new Timer();
         TimeClass timeClass = new TimeClass();
+
+        System.out.println("Key Press End Timer");
+
         //1000秒おきにtimeClassを呼び出す
         timer.scheduleAtFixedRate(timeClass,0,1000);
 
         try {
-            System.out.println("Key Press End Timer");
             System.in.read();
             timer.cancel();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+}
+
+class TimeUnit{
+
+    public int getSecondsfromSeconds(int sec){
+        return sec%60;
+    }
+
+    public int getMinutesfromSeconds(int sec){
+        return (sec/60)%60;
+    }
+
+    public int getHourfromSeconds(int sec){
+        return (sec/60)/60;
+    }
+
+
+
 }
